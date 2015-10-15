00e01694784371fcd86bb05e8e79c61bc435a6e9
lancezlin <linzhengzhong@icloud.com>
2015-10-14 22:57:21 -0700

    adding index page

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+# coding: utf-8
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-	return "Hello World"
+	return render_template("index.html")
 
 
 if __name__ == "__main__":
