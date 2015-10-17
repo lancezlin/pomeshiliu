7c9af51adcd80b4abfc691628296f79b7f2e9bfc
lancezlin <linzhengzhong@icloud.com>
2015-10-17 00:37:57 -0700

    parse walmart taxonomy api

--- db/walmart_api_tax.py
+++ db/walmart_api_tax.py
@@ -5,12 +5,22 @@ import urllib2
 tax_api_url = 'http://api.walmartlabs.com/v1/taxonomy?apiKey=f2v2kpfp5jnrk5mzp9unjjb3'
 
 while True:
-	url_open = urllib.url_open(tax_api_url)
-	data = url_open.read()
-	try:
-		js = json.loads(data)
-	except:
-		js = None
-		print 'No data retrieved!!'
+    req = urllib2.Request(tax_api_url)
+    url_open = urllib2.urlopen(req)
+    data = url_open.read()
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+        print 'No data retrieved!!'
 
-	
+    print json.dumps(js, indent=4)
+
+    for i in xrange(5):
+        catId = js["categories"][i]["id"]
+        catName = js["categories"][i]["name"]
+        print catId
+        print catName
+
+    break
+
