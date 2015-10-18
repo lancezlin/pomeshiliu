1af4a71c14c56b8e4a77a5618c5d7fa0fc67bf84
lancezlin <linzhengzhong@icloud.com>
2015-10-18 03:16:15 -0700

    improve api code to parse all sub categories and also set up tables in mysql

--- db/walmart_api_tax.py
+++ db/walmart_api_tax.py
@@ -6,7 +6,13 @@ tax_api_url = 'http://api.walmartlabs.com/v1/taxonomy?apiKey=f2v2kpfp5jnrk5mzp9u
 
 while True:
     req = urllib2.Request(tax_api_url)
-    url_open = urllib2.urlopen(req)
+    try:
+        url_open = urllib2.urlopen(req)
+    except IOError, e:
+        if hasattr(e, 'reason'):
+            print e.Reason
+        elif hasattr(e, 'code'):
+            print e.code
     data = url_open.read()
     try:
         js = json.loads(data)
@@ -16,11 +22,30 @@ while True:
 
     print json.dumps(js, indent=4)
 
-    for i in xrange(5):
-        catId = js["categories"][i]["id"]
-        catName = js["categories"][i]["name"]
-        print catId
-        print catName
-
+    for cat in js['categories']:
+        catId = cat['id']
+        catName = cat['name']
+        catPath = cat['path']
+        
+        for subcat in cat['children']:
+            subCatId = subcat['id']
+            subCatName = subcat['name']
+            subCatPath = subcat['path']
+            try:
+                for subsubcat in subcat['children']:
+                    subSubCatId = subsubcat['id']
+                    subSubCatName = subsubcat['name']
+                    subSubCatPath = subsubcat['path']
+                    print catId, catName, catPath, subCatPath, subSubCatPath    
+            except:
+                pass
+                '''try:
+                    for subsubsubcat in subsubcat['children']:
+                        subSubSubCatId = subsubsubcat['id']
+                        subSubSubCatName = subsubsubcat['name']
+                        subSubSubCatPath = subsubsubcat['path']
+                        print catId, catName, catPath, subCatPath, subSubCatPath, subSubSubCatPath    
+                except:
+                    pass'''
     break
 
